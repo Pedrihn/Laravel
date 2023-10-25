2550ee93d0a5b408b346ca5ac35329bed819a995
Pedrihn <107429880+Pedrihn@users.noreply.github.com>
2023-10-25 08:45:05 -0300

    Add files via upload

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <section class="container">
+        <h3>Home Page</h3>
+        <img src="/imagens/mk1imagem1.jpg" width="65%">
+        <br>
+        <p>
+            <a href="http://127.0.0.1:8000/produtos">Produto</a>
+            <a href="http://127.0.0.1:8000/contato">Contato</a>
+        </p>
+    </section>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ resources/views/produtos.blade.php
@@ -0,0 +1,4 @@
+
+
+<h3>jnewnbfwefwebfuwey</h3>
+<a href="http://127.0.0.1:8000">Home</a>
